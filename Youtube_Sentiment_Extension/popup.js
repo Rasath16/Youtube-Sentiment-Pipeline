8c754418eeae548b37dabcda25bed7d4f047d77b
Tharusha Rasath Hemachandra <ptrhemachandra@students.nsbm.ac.lk>
2025-12-10 12:31:07 +0530

    docker file updated

--- Youtube_Sentiment_Extension/popup.js
+++ Youtube_Sentiment_Extension/popup.js
@@ -5,7 +5,7 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const outputDiv = document.getElementById("output");
   const API_KEY = "AIzaSyAu7vHREXrcIG3UYOb2ySP6fW6m6ya6uv0"; // Your YouTube Data API key
-  const API_URL = "http://ec2-54-211-18-166.compute-1.amazonaws.com:8080";
+  const API_URL = "http://ec2-54-211-18-166.compute-1.amazonaws.com:8080/*";
 
   // Sentiment label mapping (YouTube format: -1, 0, 1)
   const SENTIMENT_LABELS = {
